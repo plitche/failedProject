b60670198967bd0ee1464a1d0c013ac5659d6258
plitche <kysu728@naver.com>
2021-09-12 23:42:20 +0900

    add gitignore

---
+++ resignationProject/src/main/java/com/project/resignation/service/TestService.java
@@ -0,0 +1,10 @@
+package com.project.resignation.service;
+
+import java.util.Map;
+
+public interface TestService {
+
+	/* 여기는 home.jsp를 호출할떄 실행시킬 메소드*/
+	public int testServiceMethod1() throws Exception;
+	
+}
