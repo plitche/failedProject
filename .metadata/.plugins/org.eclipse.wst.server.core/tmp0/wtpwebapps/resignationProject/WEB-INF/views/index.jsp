7f32787c4ec52c85bf15c1ec253703aea833798f
DESKTOP-8JMNCLE\wjdgh <wjdgh823@naver.com>
2021-09-12 22:39:18 +0900

    테이블 생성

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/resignationProject/WEB-INF/views/index.jsp
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/resignationProject/WEB-INF/views/index.jsp
@@ -1,5 +1,5 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <html>
 <head>
 	<title>Home</title>
@@ -9,7 +9,7 @@
 	Hello world!  
 </h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+<P> 안녕</P>
 
 <p>${test}</p>
 
---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/resignationProject/resources/db/drop.sql
@@ -0,0 +1,46 @@
+DROP TABLE tbSavedList;
+DROP TABLE tbAttachment;
+DROP TABLE tbShare;
+DROP TABLE tbGoodBad;
+DROP TABLE tbHit;
+DROP TABLE tbInterestField;
+DROP TABLE tbMainJob;
+DROP TABLE tbSubJob;
+DROP TABLE tbTag;
+DROP TABLE tbDistractor;
+DROP TABLE tbSubComment;
+DROP TABLE tbComment;
+DROP TABLE tbMessenger;
+DROP TABLE tbMessengerList;
+DROP TABLE tbMessengerParticipants;
+DROP TABLE tbApplyParticipants;
+DROP TABLE tbNeedParticipants;
+DROP TABLE tbSchedule;
+DROP TABLE tbIdeaVoting;
+DROP TABLE tbDiscussion;
+DROP TABLE tbStudy;
+DROP TABLE tbProject;
+DROP TABLE tbMember;
+
+DROP TABLE SEQUENCE tbDiscussion;
+DROP TABLE SEQUENCE tbIdeaVoting;
+DROP TABLE SEQUENCE tbProject;
+DROP TABLE SEQUENCE tbStudy;
+DROP TABLE SEQUENCE tbSchedule;
+DROP TABLE SEQUENCE tbNeedParticipants;
+DROP TABLE SEQUENCE tbApplyParticipants;
+DROP TABLE SEQUENCE tbMessengerList;
+DROP TABLE SEQUENCE tbMessengerParticipants;
+DROP TABLE SEQUENCE tbMessenger;
+DROP TABLE SEQUENCE tbComment;
+DROP TABLE SEQUENCE tbSubComment;
+DROP TABLE SEQUENCE tbMainJob;
+DROP TABLE SEQUENCE tbSubJob;
+DROP TABLE SEQUENCE tbTag;
+
+
+
+
+
+
+
